b05205d2d738f37c290839e8083c525b68299cdf
thiag0-dot <thiagodeginofilho@gmail.com>
2022-03-23 21:38:52 -0300

    first commit

---
+++ dias_vividos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dias_vividos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe sua idade um anos:");
+            uint anos = Convert.ToUInt16(Console.ReadLine());
+            Console.Write("Informe sua idade em meses:");
+            uint meses = Convert.ToUInt16(Console.ReadLine());
+            Console.Write("Informe sua idade em dias:");
+            uint dias = Convert.ToUInt16(Console.ReadLine());
+
+            uint vividos = (anos * 365) + (meses * 30) + dias;
+            Console.Write("O total de dias vividos é:" + vividos);
+            Console.ReadLine();
+        }
+    }
+}
